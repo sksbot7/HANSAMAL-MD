47e252feb57b0f9c3b7c700002d7b5bbd73a5d92
sksbot7 <104474096+sksbot7@users.noreply.github.com>
2024-09-15 15:20:35 +0530

    Update config.cjs

--- config.cjs
+++ config.cjs
@@ -12,14 +12,14 @@ const config = {
   ALWAYS_ONLINE: process.env.ALWAYS_ONLINE !== undefined ? process.env.ALWAYS_ONLINE === 'true' : false,
   AUTO_REACT: process.env.AUTO_REACT !== undefined ? process.env.AUTO_REACT === 'true' : false,
    /*auto block only for 212 */
-  AUTO_BLOCK: process.env.AUTO_BLOCK !== undefined ? process.env.AUTO_BLOCK === 'true' : true,
+  AUTO_BLOCK: process.env.AUTO_BLOCK !== undefined ? process.env.AUTO_BLOCK === 'false'  : false,
   
   
   REJECT_CALL: process.env.REJECT_CALL !== undefined ? process.env.REJECT_CALL === 'true' : false, 
   NOT_ALLOW: process.env.NOT_ALLOW !== undefined ? process.env.NOT_ALLOW === 'true' : true,
   MODE: process.env.MODE || "public",
-  OWNER_NAME: process.env.OWNER_NAME || "IMALKA-HANSAMAL",
-  OWNER_NUMBER: process.env.OWNER_NUMBER || "94711262551",
+  OWNER_NAME: process.env.OWNER_NAME || "S-Y-4-M",
+  OWNER_NUMBER: process.env.OWNER_NUMBER || "919888280858",
   GEMINI_KEY: process.env.GEMINI_KEY || "AIzaSyCUPaxfIdZawsKZKqCqJcC-GWiQPCXKTDc",
   WELCOME: process.env.WELCOME !== undefined ? process.env.WELCOME === 'true' : false, 
 };
