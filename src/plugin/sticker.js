1ccbfae2129d0221da58aa697d69ecea69bd42e0
sksbot7 <104474096+sksbot7@users.noreply.github.com>
2024-09-16 10:13:31 +0530

    Update sticker.js

--- src/plugin/sticker.js
+++ src/plugin/sticker.js
@@ -6,8 +6,8 @@ const stickerCommand = async (m, gss) => {
   const prefix = prefixMatch ? prefixMatch[0] : '/';
   const [cmd, arg] = m.body.startsWith(prefix) ? m.body.slice(prefix.length).split(' ') : ['', ''];
 
-  const packname = global.packname || "𝐄𝐭𝐡𝐢𝐱-𝐌𝐃";
-  const author = global.author || "🥵💫👿";
+  const packname = global.packname || "𝙎𝙆𝙎 𝘽𝙊𝙏☮︎";
+  const author = global.author || "ig|_sks_bot_";
 
   const validCommands = ['sticker', 's', 'autosticker'];
 
