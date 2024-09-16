dd5409634b6003ee8272fa10078a2da109c711db
sksbot7 <104474096+sksbot7@users.noreply.github.com>
2024-09-16 15:00:48 +0530

    Update ping.js

--- src/plugin/ping.js
+++ src/plugin/ping.js
@@ -1,15 +1,25 @@
-const { bot } = require('../lib/')
+const ping = async (m, sock) => {
+  const prefix = /^[\\/!#.]/gi.test(m.body) ? m.body.match(/^[\\/!#.]/gi)[0] : '/';
+  const cmd = m.body.startsWith(prefix) ? m.body.slice(prefix.length).toLowerCase() : '';
 
-bot(
-  {
-    pattern: 'ping ?(.*)',
-    desc: 'To check ping',
-    type: 'misc',
-  },
-  async (message, match) => {
-    const start = new Date().getTime()
-    await message.send('```Ping!```')
-    const end = new Date().getTime()
-    return await message.send('*Pong!*\n ```' + (end - start) + '``` *ms*')
+  if (cmd === "ping") {
+    const start = new Date().getTime();
+    await m.React('⚡');
+    const end = new Date().getTime();
+    const responseTime = (end - start) / 1000;
+
+    const text = `*_🔥ഉമ്പിയ SPEED: ${responseTime.toFixed(2)} s_*`;
+    const buttons = [
+    {
+        name: 'cta_url',
+                  buttonParamsJson: JSON.stringify({
+                    display_text: 'GROUP',
+             url:'https://wa.me/+919888280858?text=HI',
+  }),
+    }
+    ]
+    sock.sendMessage(m.from, { text }, { quoted: m });
   }
-)
+}
+
+export default ping;
